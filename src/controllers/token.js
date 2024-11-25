5cdbb37247f19d3448a4861e2842cf1450ad12f3
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-11-25 21:10:42 +0300

    Token MVC created

---
+++ src/controllers/token.js
@@ -0,0 +1,51 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+const Token = require("../models/token")
+
+module.exports = {
+    list: async (req, res) => {
+
+        const result = await res.getModelList(Token)
+
+        res.status(200).send({
+            error: false,
+            detail: await res.getModelListDetails(Token),
+            result
+        })
+    },
+    create: async (req, res) => {
+        const result = await Token.create(req.body)
+        res.status(201).send({
+            error: false,
+            result
+        })
+    },
+    read: async (req, res) => {
+        // console.log(req.params)
+        
+        const result = await Token.findById(req.params.id)
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async (req, res) => {
+        const result = await Token.updateOne({_id: req.params.id}, req.body, {runValidators: true})
+        res.status(202).send({
+            error: false,
+            new: await Token.findById(req.params.id),
+            result
+        })
+    },
+    deleteToken: async (req, res) => {
+        const result = await Token.deleteOne({_id: req.params.id})
+        // console.log(result)
+        
+        res.status(result.deletedCount ? 204 : 404).send({
+            error: !(result.deletedCount),
+            result
+        })
+    },
+}
---
+++ src/models/token.js
@@ -0,0 +1,28 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+
+const {Schema, model} = require("mongoose")
+
+const TokenSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "Personnel",
+        required: true,
+        index
+    },
+    token: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    }
+}, {
+    collection: "tokens",
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = model("Token", TokenSchema)
